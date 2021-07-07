24b020acd02db44747496dad009684538634450e
BrokenShell <webmaster@sharpdesigndigital.com>
2021-07-07 07:14:12 -0700

    adds SQL DB Ops

--- LambdaLib/Analysis/examples.py
+++ LambdaLib/Analysis/examples.py
@@ -1,21 +1,20 @@
 from LambdaLib.Analysis.csv_similarity import csv_similarity_score
 
 
-notes = """
-Data Analysis Examples
-- csv similarity score example
-"""
-
-# File Paths
+# Files
 true_data = "data/csv_true.csv"
 test_data = "data/csv_test.csv"
 
-# Calculates Similarity
+# Similarity
 average_score = csv_similarity_score(
     true_data,
     test_data,
 )
 
 # Output
-print(notes)
-print(f"Average Similarity Score: {100 * average_score:.2f}%")
+print("\n# Data Analysis Examples")
+print("## CSV similarity score example")
+print(f"\t• True Source: {true_data}")
+print(f"\t• Test Source: {test_data}")
+print(f"\t• Similarity Score: {100 * average_score:.2f}%")
+print("...")
